e6061bcf79d4bb3f2ab3d1777d946226b32a4cd3
lew22 <richardgarmori@gmail.com>
2023-06-07 17:44:49 -0500

    image,login and object key/values

---
+++ src/app/interfaces/Patient.ts
@@ -0,0 +1,11 @@
+export interface Patient{
+    id:string,
+    firstname:string,
+    lastname:string,
+    email:string,
+    password:string,
+    phone:string,
+    date:string,
+    gender:string,
+    img:string
+}
