30784917a8fac08b786ea7c3d5bfc64f24661cd0
HR-2019 <U201722123@upc.edu.pe>
2023-06-08 13:33:47 -0500

    Se agregó el método para eliminar o cancelar una cita

--- src/app/components/list-appointment/list-appointment.component.ts
+++ src/app/components/list-appointment/list-appointment.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-list-appointment',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-appointment.component.css']
 })
 export class ListAppointmentComponent {
+  appointmentData: any;
+
+  constructor(private http: HttpClient, public dialog: MatDialog) { }
+
+  ngOnInit() {
+    this.obtenerListaDeCitas();
+  }
+
+  obtenerListaDeCitas() {
+    const apiUrl = `https://psychohelp-open.mybluemix.net/api/v1/appointment/patient/`;
+    const id = localStorage.getItem('id');
+    this.http.get(apiUrl + id).subscribe((data: any) => {
+      this.appointmentData = data;
+    });
+  }
+  
+  eliminarCita(id: number, event: Event) {
+    event.preventDefault();
+
+    const confirmacion = confirm("¿Estás seguro de que deseas eliminar esta cita?");
+    if (!confirmacion) {
+      return;
+    }
+
+    const apiUrl = `https://psychohelp-open.mybluemix.net/api/v1/appointment/`;
+    this.http.delete(apiUrl + id).subscribe((data: any) => {
+      location.reload();
+    });
+  }
+
 
 }
