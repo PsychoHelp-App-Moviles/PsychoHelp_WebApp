5efca0cfba98df0ed26872c43205fa241c52d74d
Jorge Tuppia <70586674+Mufment@users.noreply.github.com>
2023-06-08 21:33:43 -0500

    Revert "Jorge"

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,7 +2,6 @@
 import { Component } from "@angular/core";
 import { Login } from '../../interfaces/login';
 import { LoginService } from 'src/app/services/login.service';
-import { Router } from '@angular/router'; 
 
 @Component({
   selector: "app-login",
@@ -18,65 +17,21 @@ export class LoginComponent {
 
   constructor(
     private loginService: LoginService,
-    private router: Router
   ) {}
 
   submitLogin(){
-    // this.router.navigate(['/main'])
-    this.loginService.getPatientbyEmail(this.mylogin)
-    .subscribe({
-    next: (res) =>{
-      console.log(res);
-      const entries = Object.entries(res)
-      entries.forEach(([key, value]) => {
-        if(key == 'id'){
-          localStorage.setItem('id',value)
-          localStorage.setItem('Type','Patient')
-          console.log(value)
-        }
-      });
-      if(res.email == this.mylogin.email && res.password == this.mylogin.password){
-        console.log("correct credentials PACIENT")
-        this.router.navigate(['/main'])
-      }else{
-        alert("incorrect credentials pacient")
-      }
-    },
-    error:(err) =>{
-      // alert('There was an error in retrieving data from the server')
-      console.log(err)
-    }
-    })
-
-    this.loginService.getPsychologistbyEmail(this.mylogin)
-    .subscribe({
-    next: (res) =>{
-      console.log(res);
-      const entries = Object.entries(res)
-      entries.forEach(([key, value]) => {
-        if(key == 'id'){
-          localStorage.setItem('id',value)
-          localStorage.setItem('Type','Psychologist')
-          console.log(value)
-        }
-      });
-      if(res.email == this.mylogin.email && res.password == this.mylogin.password){
-        console.log("correct credentials PSYCHOLOGIST")
-        this.router.navigate(['/main'])
-      }else{
-        alert("incorrect credentials psychologist")
-      }
-    },
-    error:(err) =>{
-      // alert('There was an error in retrieving data from the server')
-      console.log(err)
-    }
-    })
-
+    // this.loginService.verifyLogin(this.mylogin)
+    // .subscribe(
+    //   res => {
+    //     console.log(res);
+    //     this.router.navigate(['/'])
+    //   },
+    //   err => console.log(err)
+    // )
   }
 
   login() {
-    // console.log(this.mylogin.email);
-    // console.log(this.mylogin.password);
+    console.log(this.mylogin.email);
+    console.log(this.mylogin.password);
   }
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -13,41 +13,8 @@ export class LoginService {
     BASE_URL = environment.server
     constructor(private http: HttpClient) {}
 
-    //GET patient by email
-    getPatientbyEmail(login:Login):Observable<Login>{
-      return this.http.get<Login>(`${this.BASE_URL}/patients/email/${login.email}`)
-    }
-
-    //GET psychologist by email
-    getPsychologistbyEmail(login:Login):Observable<Login>{
-      return this.http.get<Login>(`${this.BASE_URL}/psychologists/email/${login.email}`)
-    }
-
-    //GETS
-    // getPatients():Observable<Login[]>{
-    //   return this.http.get<Login[]>(`${this.BASE_URL}/patients`)
-    // }
-
-    //POST
-    // createEvents(event: Event):Observable<Event>{
-    //   return this.http.post<Event>(`${this.BASE_URL}/evento/create`,event)
-    // }
-    
-    //DELETE
-    // deleteEvents(id:string){
-    //   return this.http.delete<Event>(`${this.BASE_URL}/evento/delete?eventId=${id}`)
-    //   return this.http.delete<Event>(`${this.BASE_URL}/evento/delete/${id}`)
-    // }
-
-    //UPDATE
-    // updateEvents(id: string,event: Event): Observable<Event>{
-    //   return this.http.put<Event>(`${this.BASE_URL}/evento/update/${id}`,event)
-    // }
-
-    //POST
     // verifyLogin(login: Login):Observable<Login>{
-        // return this.http.post<Login>(`${this.BASE_URL}/patients/email/${login.email}`,login)
+    //     return this.http.post<Login>(`${this.BASE_URL}/login/verify`,login)
     // }
 
 }
-
