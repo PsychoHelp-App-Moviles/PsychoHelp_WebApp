1f978434cc15c42ee88046863ba8a56202c553a1
Mufment <jorleotuppiac@gmail.com>
2023-06-08 19:15:09 -0500

    Adding "main" - home features
    Añadiendo las sugerencias y navbar al home

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,21 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { PublicationService } from 'src/app/services/publication.service';
+
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
+
 export class MainComponent implements OnInit {
 
-  constructor() { }
   isCollapsed : boolean = true;
 
+
+  publicationData:any;
+  guardado:any; 
+  constructor(private PublicationService:PublicationService) { }
   ngOnInit(): void {
+
+    this.getPublications();
+
   }
 
   getProfileRoute(): string {
     const userType = localStorage.getItem('type');
     return userType === 'Psychologist' ? '/psychologist_profile' : '/patient_profile';
   }
+
+
+  getPublications(){
+    // aca comprobamos que el usuario está guardado en el local storage
+      //this.guardado= localStorage.getItem('user');
+      //console.log(this.guardado)
+
+      this.PublicationService.getAll().subscribe(data => {
+        this.publicationData = data;
+        console.log(this.publicationData.content)
+      });   
+      
+      
+}
 
 }
