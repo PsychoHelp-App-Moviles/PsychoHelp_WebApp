f40fab067a1dd73c91521411a56a6a7e28e56811
HR-2019 <U201722123@upc.edu.pe>
2023-06-09 23:57:58 -0500

    Se agregó el componente Historial de Citas como opción en el menú principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
     component: EditProfilePatientComponent
   },
   {
-    path:"appointment_list",
+    path:"historial-citas",
     component: ListAppointmentComponent
   },
   {
